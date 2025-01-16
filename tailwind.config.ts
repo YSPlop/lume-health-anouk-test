51fe3ebd1ea5f7305ade4ee844da240d58e1c439
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2025-01-17 10:04:51 +1100

    added a comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ export default {
   ],
   theme: {
     extend: {
+      // Custom backgroun color
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
